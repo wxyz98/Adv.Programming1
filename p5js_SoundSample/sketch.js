ce3cd73443e9678471a761711992aed30dc67224
wxyz98 <32089594+wxyz98@users.noreply.github.com>
2017-09-19 16:14:35 +0800

    Update sketch.js

--- p5js_SoundSample/sketch.js
+++ p5js_SoundSample/sketch.js
@@ -4,7 +4,7 @@ var amp;
 var scale = 1.0;
 
 function setup() {
-    createCanvas(2000, 1000);
+    createCanvas(1000, 800);
     background(28, 55, 56);
     // Create an audio input and start it
     mic = new p5.AudioIn();
